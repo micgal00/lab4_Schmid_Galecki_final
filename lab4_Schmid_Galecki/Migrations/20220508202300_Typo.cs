145c6899648a261d0c4415b6c2f5c56bdd84907e
Michał Gałecki <252869@student.pwr.edu.pl>
2022-05-10 18:25:15 +0200

    Add project files.

---
+++ lab4_Schmid_Galecki/Migrations/20220508202300_Typo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lab4_Schmid_Galecki.Migrations
+{
+    public partial class Typo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RelaseDate",
+                table: "Game",
+                newName: "ReleaseDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReleaseDate",
+                table: "Game",
+                newName: "RelaseDate");
+        }
+    }
+}
---
+++ lab4_Schmid_Galecki/Migrations/20220510145819_Image.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lab4_Schmid_Galecki.Migrations
+{
+    public partial class Image : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Game",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Game");
+        }
+    }
+}
